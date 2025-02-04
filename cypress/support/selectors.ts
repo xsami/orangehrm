179715669d36449eb9f2bd05da208a10bda9121c
xsami <samil.vargas@hotmail.com>
2025-02-04 14:17:26 -0400

    test: refactor dashboard tests to use selector constants for navbar

---
+++ cypress/support/selectors.ts
@@ -0,0 +1,3 @@
+export const NAV_BAR = 'ul.oxd-main-menu > li';
+export const POST_INPUT = 'textarea.oxd-buzz-post-input';
+export const POST_FEED = '.orangehrm-buzz-newsfeed-posts > .oxd-grid-item';
